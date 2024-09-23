7f8c5790b1cbd21cdc505308465e71f2b42cdd66
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-09-23 21:23:38 +0300

    Update ServicesHome.js

--- src/components/ServicesHome/ServicesHome.js
+++ src/components/ServicesHome/ServicesHome.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./ServicesHome.module.css";
-import ServiceHome from "../ServiceHome/ServiceHome";
 import data from "./data";
 
 const ServicesHome = () => {
@@ -10,10 +9,7 @@ const ServicesHome = () => {
                 Services
             </p>
             <div>
-                <p
-                    data-aos="slide-left"
-                    className={`mx-auto ${styles.subHeading}`}
-                >
+                <p data-aos="slide-left" className={`mx-auto ${styles.subHeading}`}>
                     Modernize your workflow and maximize result
                 </p>
             </div>
@@ -22,17 +18,17 @@ const ServicesHome = () => {
                 {data &&
                     data.map((x) => (
                         <div key={x.id} className="col-lg-4 col-md-6">
-                            <ServiceHome
-                                id={x.id}
-                                heading={x.heading}
-                                content={x.content}
-                                to={x.to}
-                                icon={x.icon}
-                            />
+                            <div className={styles.serviceCard}>
+                                <img src={x.icon} alt={x.heading} className={styles.icon} />
+                                <h3 className={styles.heading}>{x.heading}</h3>
+                                <p className={styles.content}>{x.content}</p>
+                                <a href={x.to} className={styles.link}>Learn More</a>
+                            </div>
                         </div>
                     ))}
             </div>
         </div>
     );
 };
+
 export default ServicesHome;
