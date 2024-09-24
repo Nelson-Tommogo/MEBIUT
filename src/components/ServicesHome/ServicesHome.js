c4e0d5dff53b9147fcf2f696ac303e7d3c7f84df
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-09-24 08:16:47 +0300

    mebiut

--- src/components/ServicesHome/ServicesHome.js
+++ src/components/ServicesHome/ServicesHome.js
@@ -1,9 +1,6 @@
 import React from "react";
 import styles from "./ServicesHome.module.css";
-<<<<<<< HEAD
 import ServiceHome from "../ServiceHome/ServiceHome";
-=======
->>>>>>> 3b88fdcaf1e60e142cb4386658726adb48036850
 import data from "./data";
 
 const ServicesHome = () => {
@@ -13,14 +10,10 @@ const ServicesHome = () => {
                 Services
             </p>
             <div>
-<<<<<<< HEAD
                 <p
                     data-aos="slide-left"
                     className={`mx-auto ${styles.subHeading}`}
                 >
-=======
-                <p data-aos="slide-left" className={`mx-auto ${styles.subHeading}`}>
->>>>>>> 3b88fdcaf1e60e142cb4386658726adb48036850
                     Modernize your workflow and maximize result
                 </p>
             </div>
@@ -29,7 +22,6 @@ const ServicesHome = () => {
                 {data &&
                     data.map((x) => (
                         <div key={x.id} className="col-lg-4 col-md-6">
-<<<<<<< HEAD
                             <ServiceHome
                                 id={x.id}
                                 heading={x.heading}
@@ -37,22 +29,10 @@ const ServicesHome = () => {
                                 to={x.to}
                                 icon={x.icon}
                             />
-=======
-                            <div className={styles.serviceCard}>
-                                <img src={x.icon} alt={x.heading} className={styles.icon} />
-                                <h3 className={styles.heading}>{x.heading}</h3>
-                                <p className={styles.content}>{x.content}</p>
-                                <a href={x.to} className={styles.link}>Learn More</a>
-                            </div>
->>>>>>> 3b88fdcaf1e60e142cb4386658726adb48036850
                         </div>
                     ))}
             </div>
         </div>
     );
 };
-<<<<<<< HEAD
-=======
-
->>>>>>> 3b88fdcaf1e60e142cb4386658726adb48036850
 export default ServicesHome;
