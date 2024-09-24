a0e848c8e4a9c5bc60754cab0712573bd00943ed
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-09-24 08:33:17 +0300

    mebiut

--- src/components/AboutUsHome/AboutUsHome.js
+++ src/components/AboutUsHome/AboutUsHome.js
@@ -11,7 +11,7 @@ const AboutUsHome = () =>{
                 <div className="row overflow-hidden">
                     <div className="col-md-6">
                         <p data-aos="slide-right" className={styles.head}>ABOUT US</p>
-                        <p data-aos="slide-left" className={styles.heading}>Bringing Kenya’s Finest Flavors to the World</p>
+                        <p data-aos="slide-left" className={styles.heading}>Bringing Kenyas Finest Flavors to the World</p>
                         <div data-aos="zoom-in">
                             <p className={styles.content}>At **Mebiut**, we are dedicated to creating high-quality, flavorful food products that celebrate the vibrant culinary traditions of Kenya. Our team of passionate experts brings years of experience in food innovation, crafting unique sauces and condiments like our signature **Kenyan Taste sauces** and **mayonnaise**. We blend the finest local ingredients with modern techniques to provide products that enhance every meal, making them accessible to homes across Kenya and beyond.</p>
                         </div>
