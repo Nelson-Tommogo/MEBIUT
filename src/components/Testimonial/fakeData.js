445cdddd6b7d57764f33471c579ef96fe00a4f7a
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-09-24 09:24:39 +0300

    Update fakeData.js

--- src/components/Testimonial/fakeData.js
+++ src/components/Testimonial/fakeData.js
@@ -1,6 +1,9 @@
 import image1 from '../../assets/home/testimonial1.png';
 import image2 from '../../assets/home/testimonial2.png';
-import image3 from '../../assets/home/testimonial2.png';
+import image3 from '../../assets/home/testimonial3.png';
+import image4 from '../../assets/home/testimonial4.png';
+import image5 from '../../assets/home/testimonial5.png';
+import image6 from '../../assets/home/testimonial6.png';
 
 export const fakeData = [
     {
@@ -8,41 +11,41 @@ export const fakeData = [
         img: image1,
         name: 'Howard Arlene',
         profession: 'Finance Manager',
-        message: "Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business."
+        message: "MEBIUT is truly remarkable and stands behind its services completely. It's the perfect platform for our business needs, and it has significantly enhanced our operational efficiency."
     },
     {
         id: 2,
         img: image2,
         name: 'Miles Esther',
         profession: 'Public Relations Specialist',
-        message: "You won't regret it. I was amazed at the quality of it. I am really satisfied with my it."
+        message: "MEBIUT exceeded my expectations. The quality of their platform is outstanding, and I am extremely satisfied with the results."
     },
     {
         id: 3,
         img: image3,
         name: 'Pena Norma',
         profession: 'Public Relations Specialist',
-        message: "It fits our needs perfectly. Man, this thing is getting better and better as I learn more about it. You've saved our business!"
+        message: "MEBIUT fits our needs perfectly. The more I use it, the better it gets! You've truly saved our business."
     },
     {
         id: 4,
-        img: image1,
+        img: image4,
         name: 'Howard Arlene',
         profession: 'Finance Manager',
-        message: "Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business."
+        message: "MEBIUT is truly outstanding and has my full support. It's the perfect solution for our needs and has significantly improved our operations."
     },
     {
         id: 5,
-        img: image2,
+        img: image5,
         name: 'Miles Esther',
         profession: 'Public Relations Specialist',
-        message: "You won't regret it. I was amazed at the quality of it. I am really satisfied with my it."
+        message: "You won't regret choosing MEBIUT. I was amazed at the quality it offers. I am truly satisfied with my experience."
     },
     {
         id: 6,
-        img: image3,
+        img: image6,
         name: 'Pena Norma',
         profession: 'Public Relations Specialist',
-        message: "It fits our needs perfectly. Man, this thing is getting better and better as I learn more about it. You've saved our business!"
+        message: "MEBIUT fits our needs perfectly. This platform keeps getting better as I learn more about its features. You've truly transformed our business!"
     },
 ]
