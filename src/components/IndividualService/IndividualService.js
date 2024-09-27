eb73ca6b8d5c2a5acebbdc1ca33cd891d4bcdbed
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-09-27 10:45:14 +0300

    Mebiut

--- src/components/IndividualService/IndividualService.js
+++ src/components/IndividualService/IndividualService.js
@@ -9,67 +9,65 @@ import det from '../../assets/individualService/det.png';
 const IndividualService = () =>{
     const { search } = useLocation(); 
     const id = parseInt(new URLSearchParams(search).get('id'));
-    let service = data.filter(x=>{
-        return x.id === id;
-    })[0];
+    let service = data.filter(x => x.id === id)[0];
 
     useEffect(() => {
-        window.scrollTo(0, 0)
-      })
+        window.scrollTo(0, 0);
+    }, []);
 
     return(
         <>
             <div className={`container ${styles.contain} overflow-hidden`}>
-                <p data-aos='slide-right'  className={styles.heading}>{ service.heading }</p>
-                <p data-aos='slide-left' className={styles.heading_content}>{ service.content }</p>
+                <p data-aos='slide-right' className={styles.heading}>{service.heading}</p>
+                <p data-aos='slide-left' className={styles.heading_content}>{service.content}</p>
             
                 <div className="row">
                     <div className="col-md-4" data-aos='fade-right' data-aos-offset="150">
-                        <img src={timer} alt="" className={styles.img}/>
-                        <p className={styles.logo_head}>Speed Optimization</p>
-                        <p className={styles.heading_content}>Drink the Kool-aid shoot me an email nor thinking outside the box</p>
+                        <img src={timer} alt="Speed Optimization" className={styles.img}/>
+                        <p className={styles.logo_head}>Flavor Innovation</p>
+                        <p className={styles.heading_content}>Crafting unique flavors that elevate every dish, making your meals unforgettable.</p>
                     </div>
 
                     <div className="col-md-4" data-aos='fade' data-aos-offset="150">
-                        <img src={det} alt="" className={styles.img} />
-                        <p className={styles.logo_head}>Marketing Strategy</p>
-                        <p className={styles.heading_content}>Touch base nail jelly to the hothouse what about scaling components</p>
+                        <img src={det} alt="Marketing Strategy" className={styles.img} />
+                        <p className={styles.logo_head}>Custom Recipes</p>
+                        <p className={styles.heading_content}>Our team specializes in developing tailored sauces to fit your culinary vision.</p>
                     </div>
 
                     <div className="col-md-4" data-aos='fade-left' data-aos-offset="150">
-                        <img src={lock} alt="" className={styles.img}/>
-                        <p className={styles.logo_head}>Experts Worker</p>
-                        <p className={styles.heading_content}>Parallel path it's a simple lift and shift the team pull in ten extra bodies </p>
+                        <img src={lock} alt="Experts Worker" className={styles.img}/>
+                        <p className={styles.logo_head}>Quality Assurance</p>
+                        <p className={styles.heading_content}>We ensure every bottle meets the highest standards of quality and flavor.</p>
                     </div>
                 </div>
 
                 {
-                    service.contents.map((x, idx) =>(
-                        idx%2 === 0 ?
+                    service.contents.map((x, idx) => (
+                        idx % 2 === 0 ? 
                         <div key={idx} className="row" style={{marginTop: '112px'}}>
                             <div data-aos='slide-right' data-aos-offset="150" className="col-md-6">
-                                <img src={x.img} alt="" className={styles.contentImg}/>
+                                <img src={x.img} alt={x.heading} className={styles.contentImg}/>
                             </div>
                             <div data-aos='slide-left' data-aos-offset="150" className="col-md-6">
-                                <p className={`${styles.heading} ${styles.subHead}`}>{ x.heading }</p>
-                                <p className={`${styles.heading_content} ${styles.justify}`}>{ x.content }</p>
+                                <p className={`${styles.heading} ${styles.subHead}`}>{x.heading}</p>
+                                <p className={`${styles.heading_content} ${styles.justify}`}>{x.content}</p>
                             </div>
                         </div>
                         :
                         <div key={idx} className="row" style={{marginTop: '112px'}}>
                             <div className="col-md-6" data-aos='slide-right' data-aos-offset="150">
-                                <p className={`${styles.heading} ${styles.subHead}`}>{ x.heading }</p>
-                                <p className={`${styles.heading_content} ${styles.justify}`}>{ x.content }</p>
+                                <p className={`${styles.heading} ${styles.subHead}`}>{x.heading}</p>
+                                <p className={`${styles.heading_content} ${styles.justify}`}>{x.content}</p>
                             </div>
                             <div className="col-md-6" data-aos='slide-left' data-aos-offset="150">
-                                <img src={x.img} alt="" className={styles.contentImg}/>
+                                <img src={x.img} alt={x.heading} className={styles.contentImg}/>
                             </div>
                         </div>
                     ))
                 }
-            
             </div>
         </>
-    )
+    );
 }
+
 export default IndividualService;
--- src/components/SMediaServices/SMediaServices.js
+++ src/components/SMediaServices/SMediaServices.js
@@ -4,7 +4,7 @@ import img1 from '../../assets/sMediaService/main.png';
 import img2 from '../../assets/sMediaService/middle1.png';
 import img3 from '../../assets/sMediaService/bottom1.png';
 
-const SMediaServices = () =>{
+const SMediaServices = () => {
     return (
         <>
             <div>
@@ -12,14 +12,16 @@ const SMediaServices = () =>{
                 <div className="row mt-5 mx-0 flex-column-reverse flex-md-row">
                     <div className="col-md-6 px-0">
                         <div className={styles.topContain}>
-                            <p className={`${styles.headTop}`}>Social <span style={{color:'#ff4c0a'}}>Media</span></p>
-                            <p className={styles.headTop}>Marketing</p>
-                            <p className={styles.identityTop}>Identity on Social Media</p>
-                            <p className={styles.content}>More than ever users are seeking information and basing decisions on their peers’ advice and opinions. Customers head to your Facebook and Twitter sometimes based on a post or a re-tweet. Let your brand identity on social media be consistent with the brand you have worked so hard to develop.</p>
+                            <p className={`${styles.headTop}`}>Custom <span style={{ color: '#ff4c0a' }}>Sauce</span></p>
+                            <p className={styles.headTop}>Development Services</p>
+                            <p className={styles.identityTop}>Crafting Unique Flavors</p>
+                            <p className={styles.content}>
+                                In the world of culinary delights, having a unique sauce can set your dishes apart. Our custom sauce development service focuses on creating delicious, high-quality sauces tailored to your specific needs and local taste preferences. Whether it's a spicy peri-peri, tangy mango, or a savory barbecue sauce, we ensure every recipe embodies the essence of Kenyan cuisine.
+                            </p>
                         </div>
                     </div>
                     <div className="col-md-6 px-0">
-                        <img src={img1} alt="" className={styles.img1}/>
+                        <img src={img1} alt="Custom Sauce Development" className={styles.img1} />
                     </div>
                 </div>
 
@@ -27,11 +29,13 @@ const SMediaServices = () =>{
                 <div className="container">
                     <div className={`row ${styles.contain} flex-column-reverse flex-md-row`}>
                         <div className={`col-md-6 ${styles.middleContain}`}>
-                            <p className={styles.heading}>Next Level</p>
-                            <p className={styles.text}>Bringing your business into the social media scene will bring you to the next level of advertising as well as a whole new way to connect with your market. This is the next step.</p>
+                            <p className={styles.heading}>Elevate Your Culinary Offerings</p>
+                            <p className={styles.text}>
+                                By incorporating custom sauces into your menu, you can enhance flavors and create memorable dining experiences. Our expert team collaborates with you to design sauces that align with your brand and cater to the preferences of your customers, ensuring that your dishes not only taste amazing but also stand out in the competitive market.
+                            </p>
                         </div>
                         <div className="col-md-6">
-                            <img src={img2} alt="" className={styles.img2}/>
+                            <img src={img2} alt="Elevate Culinary Offerings" className={styles.img2} />
                         </div>
                     </div>
                 </div>
@@ -40,18 +44,19 @@ const SMediaServices = () =>{
                 <div className="container">
                     <div className={`row ${styles.contain}`}>
                         <div className="col-md-6">
-                            <img src={img3} alt="" className={styles.img2}/>
+                            <img src={img3} alt="Tailored Sauce Development" className={styles.img2} />
                         </div>
                         <div className={`col-md-6 ${styles.bottomContain}`}>
-                            <p className={styles.heading1}>Custom Approach</p>
-                            <p className={styles.text1}>Our team will help you establish a presence on both Facebook and Twitter with custom designed graphical elements that will make a lasting impression on all who visit your page. Combine this visibility with the ability to link directly to your website and you have a recipe for major exposure and traffic.</p>
+                            <p className={styles.heading1}>Tailored Solutions for Your Brand</p>
+                            <p className={styles.text1}>
+                                Our custom sauce development services are designed to meet the unique needs of your culinary business. From concept to creation, we work closely with you to ensure your sauces reflect your brand identity and culinary vision. Together, we can create flavors that resonate with your audience, increase customer loyalty, and drive sales.
+                            </p>
                         </div>
-                        
                     </div>
                 </div>
-
             </div>
         </>
     )
 }
+
 export default SMediaServices;
