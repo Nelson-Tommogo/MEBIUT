307b3a03a70c38a273bdf425d5924b36884c4372
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-09-27 08:57:09 +0300

    Mebiut

--- src/components/ChoosingUs/ChoosingUs.js
+++ src/components/ChoosingUs/ChoosingUs.js
@@ -1,37 +1,36 @@
 import React from "react";
 import styles from './ChoosingUs.module.css';
-import experience from '../../assets/home/experience.png';
-import communication from '../../assets/home/communication.png';
-import comfort from '../../assets/home/comfort.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils, faThumbsUp, faSmile } from '@fortawesome/free-solid-svg-icons';
 
-const ChoosingUs = () =>{
-    return(
-        <div style={{backgroundColor: '#fff1e5'}}>
+const ChoosingUs = () => {
+    return (
+        <div style={{ backgroundColor: '#fff1e5' }}>
             <div className="container overflow-hidden">
-                <p data-aos='slide-right' data-aos-offset="220" className={styles.heading}>Top three the reason why you should choosing us.</p>
-                <p data-aos='slide-left' className={styles.sub_text}>In quo quaerimus, non numquam eius modi tempora incidunt</p>
+                <p data-aos='slide-right' data-aos-offset="220" className={styles.heading}>Top three reasons to choose Mebiut Company.</p>
+                <p data-aos='slide-left' className={styles.sub_text}>Experience the perfect blend of Kenyan flavors in every bite.</p>
                 <div className={`row ${styles.sub}`}>
-                    <div data-aos='fade-right' className="col-md-4">
-                        <img src={experience} alt="experience"/>
-                        <p className={styles.logo_head}>Experience</p>
-                        <p className={styles.logo_text}>Probabo, inquit, modo ista sis aequitate, quam interrogare aut in voluptate ponit.</p>
+                    <div data-aos='fade-right' className="col-md-4 text-center">
+                        <FontAwesomeIcon icon={faUtensils} size="3x" className={styles.icon} />
+                        <p className={styles.logo_head}>Authentic Kenyan Taste</p>
+                        <p className={styles.logo_text}>Our sauces capture the essence of Kenyan cuisine, providing unique flavors that resonate with local tastes.</p>
                     </div>
 
-                    <div data-aos='fade' className="col-md-4">
-                        <img src={communication} alt="experience"/>
-                        <p className={styles.logo_head}>Easy Communication</p>
-                        <p className={styles.logo_text}>Probabo, inquit, modo ista sis aequitate, quam nihil impedit, quo.</p>
+                    <div data-aos='fade' className="col-md-4 text-center">
+                        <FontAwesomeIcon icon={faThumbsUp} size="3x" className={styles.icon} />
+                        <p className={styles.logo_head}>Quality Mayonnaise</p>
+                        <p className={styles.logo_text}>Enjoy our creamy mayonnaise made with the finest ingredients, perfect for enhancing your meals.</p>
                     </div>
 
-                    <div data-aos='fade-left' className="col-md-4">
-                        <img src={comfort} alt="experience"/>
-                        <p className={styles.logo_head}>Comfortable</p>
-                        <p className={styles.logo_text}>Alii autem, quibus ego cum teneam sententiam, quid bonum esse fugiendum.</p>
+                    <div data-aos='fade-left' className="col-md-4 text-center">
+                        <FontAwesomeIcon icon={faSmile} size="3x" className={styles.icon} />
+                        <p className={styles.logo_head}>Customer Satisfaction</p>
+                        <p className={styles.logo_text}>We prioritize your experience, ensuring our products meet your expectations for quality and flavor.</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default ChoosingUs;
